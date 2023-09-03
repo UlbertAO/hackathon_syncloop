f9fbeee3dd42ad82f46b5f10fd3d9ba04e196859
ulbertAO <sp595993@gmail.com>
2023-09-03 22:09:11 +0530

    initial  commit

---
+++ Public/js/action.js
@@ -0,0 +1,78 @@
+
+window.onload = extraction;
+
+const PlaylistId = document.getElementById('PlaylistId');
+// Listen for input in the name field
+PlaylistId.addEventListener('input', () => {
+    updateTitle.disabled = !PlaylistId.value; // Enable the submit button if the input has a value
+});
+
+const updateTitle = document.getElementById('updateTitle');
+updateTitle.addEventListener('click', updateTitleCB);
+
+
+function extraction() {
+    // extract params and playlistid
+
+    const url = window.location.href;
+    const urlParams = new URLSearchParams(url.split('#')[1]); // Extract parameters after the hash (#) symbol
+
+    const accessTokenParam = urlParams.get('access_token');
+
+    const PlaylistIdVal  = localStorage.getItem('PlaylistId');
+    PlaylistId.value = PlaylistIdVal;
+    if(PlaylistId.value){
+        updateTitle.disabled = !PlaylistId.value;
+    }
+
+    if (accessTokenParam && PlaylistId.value) {
+        window.history.replaceState(null, '', url.split('?')[0].split('#')[0]);
+        updateTitle.disabled=true;
+        updateTitle.textContent="Updating Title..."
+
+        const apiUrl = url.split('?')[0].split('#')[0] + 'api/updateTitle';
+        const bodyData = {
+            playlistId:PlaylistId.value,
+            token:accessTokenParam
+        }
+
+        fetch(apiUrl, {
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(bodyData)
+            })
+            .then(response => response.json())
+            .then(data => {
+                updateTitle.disabled=false;
+                updateTitle.textContent="Update Title"
+
+                document.getElementById('result').textContent = "Title Updated Successfully. Click on the below link to view your video."
+                const vidId=data?.result?.jsonDoc?.id
+                if(vidId){
+                document.getElementById('ytLink').textContent = "https://www.youtube.com/watch?v=" + vidId;
+                document.getElementById('ytLink').href = "https://www.youtube.com/watch?v=" + vidId;
+
+                }
+
+            })
+            .catch(error => {
+                updateTitle.disabled=false;
+                updateTitle.textContent="Update Title"
+
+                console.error('Error fetching data:', error);
+            });
+    } 
+}
+
+function updateTitleCB(){
+    const PlaylistIdVal = PlaylistId.value;
+    if (PlaylistIdVal) {
+        localStorage.setItem('PlaylistId', PlaylistIdVal);
+        // alert(`PlaylistId "${PlaylistIdVal}" has been stored in local storage.`);
+        window.location="https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/youtube&state=state_parameter_passthrough_value&redirect_uri=http://localhost:5001&response_type=token&client_id=644444506873-u1f31kni740e6575ek5ofa55opmvtj8i.apps.googleusercontent.com"
+    } else {
+        alert('Please enter PlaylistId.');
+    }
+}
---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+// Load environment variables from .env file
+dotenv.config({path:".env"})
+const port = process.env.PORT || 5000;
+
+// create express server
+const app = express();
+
+app.use(express.static(path.join(__dirname,"Public")));
+
+// app.use(express.json());
+app.use(bodyParser.json());
+console.log(__dirname);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('pages/index.html'));
+  });
+
+app.post('/api/updateTitle',async (req,res)=>{
+    try {
+        const { playlistId, token } = req.body;
+        yt_api_key = process.env.YT_API_KEY
+        if(!yt_api_key){
+            throw new Error("You Tube API key not found")
+        }
+
+        if (!playlistId || !token) {
+            return res.status(400).json({ error: 'Both playlistId and access_token are required.' });
+        }
+
+        const apiUrl = process.env.SYNCLOOP_URL;
+        const syncloop_token = process.env.SYNCLOOP_TOKEN;
+        if(!apiUrl || !syncloop_token){
+            throw new Error("SyncLoop URL or token required")
+        }
+        const bodyData = {
+            playlistId:playlistId,
+            token:token,
+            ytAPIkey:yt_api_key
+        }
+        const headers = {
+            'Authorization': `Bearer ${syncloop_token}`,
+            'Content-Type': 'application/json', 
+        };
+
+        axios.post(apiUrl, bodyData,{headers})
+        .then(response => {
+            res.status(response.status).json(response.data);
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'An error occurred while making the PUT request.' });
+        });
+
+    } catch (error) {
+        console.error('Error making PUT request:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+})
+// start express server
+app.listen(port,()=>{
+    console.log(`App is running on http://localhost:${port}`);
+    console.log('Press CTRL-C to stop.');
+})
